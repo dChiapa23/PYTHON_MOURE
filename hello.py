5601272f351007e0eb77e6d110d6cbb0acb4d272
dchiapa <dchiapa@frba.utn.edu.ar>
2024-05-30 21:33:31 -0300

    tipos de datos basicos

--- hello.py
+++ hello.py
@@ -11,3 +11,9 @@ lineas
 #Conocer el tipo de dato de algo
 
 print(type("Hola mundo")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(5.10)) # Tipo 'float'
+print(type(5 + 10j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+
+
