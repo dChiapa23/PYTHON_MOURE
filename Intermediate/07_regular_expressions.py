3bcafd62abe81a001145b64aa4ff67aa55bc5baa
dchiapa <dchiapa@frba.utn.edu.ar>
2024-06-17 19:19:02 -0300

    add regular expressions file

---
+++ Intermediate/07_regular_expressions.py
@@ -0,0 +1,47 @@
+# Expresiones regulares
+
+import re
+
+my_string = "Esta es la lección numero 7: Lección Expresiones regulares"
+my_other_string = "Esta no es la lección numero 6: Lección Manejo de ficheros"
+
+# Match
+match = re.match("Esta es la lección", my_string)
+span = match.span()
+
+
+print(match)
+print(span)
+print(my_string[match.start():match.end()])
+
+
+match = re.match("Esta no es la lección", my_other_string)
+# if not(match == None):
+# is match is not None:
+if match != None:
+  print(match)
+  print(span)
+  print(my_other_string[match.start():match.end()])
+
+# Search
+search = re.search("lección", my_string, re.I)
+print(search)
+print(my_string[search.start():search.end()])
+
+# Findall
+findall = re.findall("es", my_string)
+print(findall)
+
+# Split
+split = re.split(" ", my_string)
+print(split)
+
+# Sub
+sub = re.sub("a", "y", my_string)
+print(sub)
+sub = sub = re.sub("lección|Lección", "LECCIÓN", my_string)
+print(sub)
+sub = sub = re.sub("[L|l]ección", "LECCIÓN", my_string)
+print(sub)
+
+# 
