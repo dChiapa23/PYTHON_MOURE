b96186ada7c284875c094d3fa1b8b275739687f6
dchiapa <dchiapa@frba.utn.edu.ar>
2024-06-17 23:11:30 -0300

    update regular expression file

--- Intermediate/07_regular_expressions.py
+++ Intermediate/07_regular_expressions.py
@@ -44,4 +44,27 @@ print(sub)
 sub = sub = re.sub("[L|l]ección", "LECCIÓN", my_string)
 print(sub)
 
-# 
+# Patrones
+pattern = r"[Ll]ección"
+print(re.findall(pattern, my_string))
+
+pattern = r"[Ll]ección|Expresiones"
+print(re.findall(pattern, my_string))
+
+pattern = r"[a-z]"
+print(re.findall(pattern, my_string))
+
+pattern = r"[0-9]"
+print(re.findall(pattern, my_string))
+
+pattern = r"\d"
+print(re.findall(pattern, my_string))
+
+pattern = r"\D"
+print(re.findall(pattern, my_string))
+
+pattern = r"[l]."
+print(re.findall(pattern, my_string))
+
+pattern = r"[l].*"
+print(re.findall(pattern, my_string))
